40a3ebade46605d85592b7a0faf770f4d9724865
Stepan Tarasenko <st.a.tarasenko@gmail.com>
2020-11-08 15:14:53 +0200

    Add prediction method and iteration through all inputs, add data storage object

--- src/main/java/MetaData.java
+++ src/main/java/MetaData.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class MetaData {
+    private String word;
+    private double probability;
+
+    public MetaData(String word, double probability) {
+        this.word = word;
+        this.probability = probability;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
+
+    @Override
+    public String toString() {
+        return "MetaData{" +
+                "word='" + word + '\'' +
+                ", probability=" + probability +
+                '}';
+    }
 }
--- src/main/java/TextPredictor.java
+++ src/main/java/TextPredictor.java
@@ -11,6 +11,30 @@ public class TextPredictor {
     public void main() throws IOException, URISyntaxException {
         initAlphabet();
         initInputArrays();
+        bigramsProbability = Collections.emptyMap();
+
+        for (Map.Entry<int[][], Double> picture : inputPictures.entrySet()) {
+            System.out.println("Picture length: " + picture.getKey()[0].length);
+            System.out.println("Picture noise: " + picture.getValue());
+            initGraph(picture.getKey(), picture.getValue());
+            predictTextOnPicture(picture.getKey(), picture.getValue());
+            printPredictedText(picture);
+        }
+    }
+
+    private void printPredictedText(Map.Entry<int[][], Double> picture) {
+        int lastIndex = picture.getKey()[0].length - 1;
+        double maxProb = -Double.MAX_VALUE;
+        for (Map.Entry<String, MetaData> entry : graph.get(lastIndex).entrySet()){
+            if (entry.getValue().getProbability() > maxProb) {
+                maxProb = entry.getValue().getProbability();
+            }
+        }
+        for (Map.Entry<String, MetaData> entry : graph.get(lastIndex).entrySet()){
+            if (entry.getValue().getProbability() == maxProb) {
+                System.out.println(entry.getKey() + entry.getValue().getWord());
+            }
+        }
     }
 
     private void initAlphabet() throws IOException, URISyntaxException {
@@ -83,5 +107,47 @@ public class TextPredictor {
             graph.get(currentWidth - 1).put(entry.getKey(), new MetaData("", probability));
         }
         System.out.println(graph.toString());
+    }
+
+    public void predictTextOnPicture(int[][] picture, double noiseProbability) {
+        for (int i = 0; i < picture[0].length; i++) {
+            for (Map.Entry<String, int[][]> letter : alphabet.entrySet()) {
+                int currentWidth = letter.getValue()[0].length;
+                if (i < currentWidth || graph.get(i - currentWidth).isEmpty()) {
+                    continue;
+                }
+                double maxProb = -Double.MAX_VALUE;
+                String bestSequence = "";
+                for (Map.Entry<String, MetaData> node : graph.get(i - currentWidth).entrySet()) {
+                    double prevProbability = node.getValue().getProbability();
+                    String prevString = node.getValue().getWord();
+
+                    int[][] currentCharacter = new int[letter.getValue().length][];
+                    for (int j = 0; j < currentCharacter.length; j++) {
+                        currentCharacter[j] = Arrays.copyOfRange(picture[j], i - currentWidth + 1, i + 1);
+                    }
+
+                    double currentProbability = calculateProbability(
+                            currentCharacter,
+                            letter.getValue(),
+                            noiseProbability
+                    );
+
+                    double newProbability = currentProbability + prevProbability;
+
+                    if (!bigramsProbability.isEmpty()) {
+                        double pb = bigramsProbability.get(node.getKey() + letter.getKey());
+                        newProbability += Math.log(pb);
+                    }
+
+                    if (newProbability > maxProb) {
+                        maxProb = newProbability;
+                        bestSequence = prevString + letter.getKey();
+                    }
+
+                    graph.get(i).put(letter.getKey(), new MetaData(bestSequence, maxProb));
+                }
+            }
+        }
     }
 }
